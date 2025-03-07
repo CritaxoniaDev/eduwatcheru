1c0e6979aa776f76be59c13c7b69eebb4374d3ec
Gian Alcantara <raphaelalcantara51@gmail.com>
2025-03-07 23:19:17 +0800

    init commit

---
+++ ezwatch/config.js
@@ -0,0 +1,5 @@
+const config = {
+    apiKey: atob('NTJmZjQ2OWFhN2IyYzhiYjNlZjBkMmI3NzQ4NTE2MGY='),
+    baseUrl: 'https://api.themoviedb.org/3',
+    imageBase: 'https://image.tmdb.org/t/p/w500'
+};
